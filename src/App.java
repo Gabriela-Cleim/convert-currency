2bbe75556e54666411fdb5ea141920a91a878ba8
Gabriela <gabriela.cleims@gmail.com>
2024-04-17 21:44:13 -0300

    second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 
+/*
+ *Conversor de Moeda: Escreva um programa que converta uma quantia em uma moeda para 
+ *outra moeda (por exemplo, euros, libras, ienes) com base em 
+ *uma taxa de câmbio fornecida pelo usuário.
+ */
+
+
 public class App {
     public static void main(String[] args) throws Exception {
 
@@ -16,6 +23,8 @@ public class App {
 
         double result = currencyValue * currencyFee;
         System.out.println("O valor convertido para o moeda escolhida é: " + result);
+
+        scanner.close();
 
     }
 }
